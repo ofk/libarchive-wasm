904f5901cd31543f7df64c194143d81b15bb0f3c
ofk <ofkjpn+github@gmail.com>
2023-07-24 11:15:45 +0900

    Upgrade libraries and fix Dockerfile

--- lib/build.bash
+++ lib/build.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 emcc ./libarchive.c \
-	-I /usr/local/include/ /usr/local/lib/libarchive.a /usr/local/lib/liblzma.a /usr/local/lib/libssl.a /usr/local/lib/libcrypto.a \
+	-I /usr/local/include /usr/local/lib/libarchive.a \
+	/usr/local/lib/libz.a /usr/local/lib/libbz2.a /usr/local/lib/liblzma.a /usr/local/lib/libssl.a /usr/local/lib/libcrypto.a \
 	-o ./build/libarchive.js \
-	-s USE_ZLIB=1 -s USE_BZIP2=1 -s MODULARIZE=1 -s EXPORT_NAME=libarchive -s WASM=1 -O3 -s ALLOW_MEMORY_GROWTH=1 \
-	-s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' -s EXPORTED_FUNCTIONS=@./exported_functions.json \
+	-s MODULARIZE=1 -s EXPORT_NAME=libarchive -s WASM=1 -O3 -s ALLOW_MEMORY_GROWTH=1 \
+	-s EXPORTED_RUNTIME_METHODS='["cwrap"]' -s EXPORTED_FUNCTIONS=@./exported_functions.json \
 	-s ERROR_ON_UNDEFINED_SYMBOLS=0

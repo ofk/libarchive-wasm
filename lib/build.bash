ba680952a6daa23f78bfc27f4f1f93d50b117ab9
ofk <ofkjpn+github@gmail.com>
2025-03-15 16:06:11 +0900

    Update image

--- lib/build.bash
+++ lib/build.bash
@@ -4,6 +4,6 @@ emcc ./libarchive.c \
 	-I /usr/local/include /usr/local/lib/libarchive.a \
 	/usr/local/lib/libz.a /usr/local/lib/libbz2.a /usr/local/lib/liblzma.a /usr/local/lib/libssl.a /usr/local/lib/libcrypto.a \
 	-o ./build/libarchive.js \
-	-s MODULARIZE=1 -s EXPORT_NAME=libarchive -s WASM=1 -O3 -s ALLOW_MEMORY_GROWTH=1 \
+	-s MODULARIZE=1 -s EXPORT_NAME=libarchive -s WASM=1 -s WASM_BIGINT=0 -O3 -s ALLOW_MEMORY_GROWTH=1 \
 	-s EXPORTED_RUNTIME_METHODS='["cwrap"]' -s EXPORTED_FUNCTIONS=@./exported_functions.json \
 	-s ERROR_ON_UNDEFINED_SYMBOLS=0

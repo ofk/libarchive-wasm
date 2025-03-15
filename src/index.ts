241734db4be25c71e6c2b5124c99b3632bc8358e
ofk <ofkjpn+github@gmail.com>
2025-03-15 17:50:31 +0900

    Remove blank

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@ export * from './ArchiveReader';
 export type { LibarchiveModule } from './libarchive';
 export { default as libarchive } from './libarchive';
 export * from './libarchiveWasm';
-
 export * from './wrapLibarchiveWasm';
